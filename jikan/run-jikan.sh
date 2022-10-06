00eec7ec4bbdde747117f22dd7896e0fb7ff2f8a
Angel Garcia <32900272+a-garcia-alvarez@users.noreply.github.com>
2022-10-06 16:06:57 +0200

    Update project to make it work with latest v3 (#6)
    - updated php version
    - changed artisan config (must be reviwed later)
    - changed internal port
    - some other fast made things that should be fixed later
    
    Co-authored-by: Angel Garcia <a-garcia-alvarez@users.noreply.github.com>

--- jikan/run-jikan.sh
+++ jikan/run-jikan.sh
@@ -3,4 +3,5 @@
 chmod -R a+w storage/
 
 # php:7.3.14-apache-buster ENTRYPOINT
-docker-php-entrypoint apache2-foreground
+#docker-php-entrypoint apache2-foreground
+php artisan serve --host 0.0.0.0 --port=8080
